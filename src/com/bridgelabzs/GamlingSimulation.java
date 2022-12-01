1f9a67aa51f4374b5e80ef060cd8a644c0aa0539
9663461085 <ratansollapure16310@gmail.com>
2022-12-01 12:53:08 +0530

    Added:Checking condition for resing a day

--- src/com/bridgelabzs/GamlingSimulation.java
+++ src/com/bridgelabzs/GamlingSimulation.java
@@ -10,22 +10,32 @@ public class GamlingSimulation {
 	 * 
 	 * @param args
 	 */
-	public void startGambling() {
-		int startingAmount = 100;
-		int bet_Per_Game = 1;
-		int bet_result = (int) (Math.random() * 10) % 2;
-		if (bet_result == 0) {
-			startingAmount -= 1;
-			System.out.println("Gambler lost amount left = " + startingAmount);
-		} else {
-			startingAmount += 1;
-			System.out.println("Gambler won amount left =" + startingAmount);
 
-		}
-	}
-
-	public static void main(String[] args) {
-		GamlingSimulation play = new GamlingSimulation();
-		play.startGambling();
-	}
+    public void startGambling() {
+        int startingAmount=100;
+        int bet_Per_Game = 1;
+        /*
+         * Checking condition with while loop
+         */
+        while ( startingAmount != 150 || startingAmount !=50) {
+            int bet_result = (int) (Math.random() * 10) % 2;
+            if (bet_result == 0) {
+                startingAmount -= bet_Per_Game;
+            } else {
+                startingAmount += bet_Per_Game;
+            }
+            if(startingAmount == 150) {
+                System.out.println("Won 50% of the stake ");
+                break;}
+            else if (startingAmount == 50)
+            {
+                System.out.println("Lost 50 % of the stake");
+                break;}
+        }
+    }
+    public static void main (String[]args){
+    	GamlingSimulation play = new GamlingSimulation();
+        play.startGambling();
+    }
 }
+
