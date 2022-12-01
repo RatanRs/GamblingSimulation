604c90bd2d6a3921241223e9ca35e56b7cb9c812
9663461085 <ratansollapure16310@gmail.com>
2022-12-01 12:38:13 +0530

    Added:Displayed Starting amount and bet

--- src/com/bridgelabzs/GamlingSimulation.java
+++ src/com/bridgelabzs/GamlingSimulation.java
@@ -1,5 +1,23 @@
 package com.bridgelabzs;
 
+/***
+ * 
+ * @author Admin
+ *
+ */
 public class GamlingSimulation {
+	/***
+	 * 
+	 * @param args
+	 */
+	public static void startGambling() {
+		int startingAmount = 100;
+		int bet_Per_Game = 1;
+		System.out.println("starting amount=" + startingAmount);
+		System.out.println("bet per game=" + bet_Per_Game);
+	}
 
+	public static void main(String args[]) {
+		startGambling();
+	}
 }
