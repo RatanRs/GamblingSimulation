2dd71367747b387bcad63e25a281a58fed88b1ed
9663461085 <ratansollapure16310@gmail.com>
2022-12-01 12:47:23 +0530

    Added:Checking gambler won or lost

--- src/com/bridgelabzs/GamlingSimulation.java
+++ src/com/bridgelabzs/GamlingSimulation.java
@@ -10,14 +10,22 @@ public class GamlingSimulation {
 	 * 
 	 * @param args
 	 */
-	public static void startGambling() {
+	public void startGambling() {
 		int startingAmount = 100;
 		int bet_Per_Game = 1;
-		System.out.println("starting amount=" + startingAmount);
-		System.out.println("bet per game=" + bet_Per_Game);
+		int bet_result = (int) (Math.random() * 10) % 2;
+		if (bet_result == 0) {
+			startingAmount -= 1;
+			System.out.println("Gambler lost amount left = " + startingAmount);
+		} else {
+			startingAmount += 1;
+			System.out.println("Gambler won amount left =" + startingAmount);
+
+		}
 	}
 
-	public static void main(String args[]) {
-		startGambling();
+	public static void main(String[] args) {
+		GamlingSimulation play = new GamlingSimulation();
+		play.startGambling();
 	}
 }
