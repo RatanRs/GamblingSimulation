e6c39b5b21f233277638ecee86468ec81c1dda28
9663461085 <ratansollapure16310@gmail.com>
2022-12-01 13:22:00 +0530

    Added:Checking to continue play or stop

--- src/com/bridgelabzs/GamlingSimulation.java
+++ src/com/bridgelabzs/GamlingSimulation.java
@@ -11,26 +11,19 @@ public class GamlingSimulation {
 	 * @param args
 	 */
 
-	public int startGambling() 
-	{
+	public int startGambling() {
 		int startingAmount = 100;
 		int bet_Per_Game = 1;
-		while (startingAmount != 150 || startingAmount != 50)
-		{
+		while (startingAmount != 150 || startingAmount != 50) {
 			int bet_result = (int) (Math.random() * 10) % 2;
-			if (bet_result == 0)
-			{
+			if (bet_result == 0) {
 				startingAmount -= bet_Per_Game;
-			} 
-			else
-			{
+			} else {
 				startingAmount += bet_Per_Game;
 			}
-			if (startingAmount == 150) 
-			{
+			if (startingAmount == 150) {
 				break;
-			} else if (startingAmount == 50)
-			{
+			} else if (startingAmount == 50) {
 				break;
 			}
 		}
@@ -40,7 +33,7 @@ public class GamlingSimulation {
 	public void playFor20Days() {
 		int total_Amount_Invested_In_20_Days = 2000;
 		int amount_After_20Days = 0;
-
+		int profit = 0;
 		for (int i = 1; i <= 20; i++) {
 			int result = startGambling();
 			amount_After_20Days += result;
@@ -53,7 +46,7 @@ public class GamlingSimulation {
 		}
 		System.out.println("Amount after 20 day is " + amount_After_20Days);
 		if (amount_After_20Days > total_Amount_Invested_In_20_Days) {
-			int profit = amount_After_20Days - total_Amount_Invested_In_20_Days;
+			profit = amount_After_20Days - total_Amount_Invested_In_20_Days;
 			System.out.println("Total profit after 20 days is = " + profit);
 
 		} else if (amount_After_20Days < total_Amount_Invested_In_20_Days) {
@@ -61,6 +54,12 @@ public class GamlingSimulation {
 			System.out.println("Total loss after 20 days is = " + loss);
 		} else
 			System.out.println("No profit No loss");
+		if (amount_After_20Days > total_Amount_Invested_In_20_Days) {
+			System.out.println("===============================================");
+			System.out.println("Won " + profit + "$ Last month. Play Again");
+			playFor20Days();
+		}
+
 	}
 
 	public static void main(String[] args) {
